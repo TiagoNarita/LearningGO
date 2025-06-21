ba0727758ebe258b5833168de318519389825e4e
Tiago Narita <tiagobnarita@gmail.com>
2025-06-21 20:23:23 -0300

    Integer package add

---
+++ Begin/helloworld/hello.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+const englishHelloPrefix = "Hello, "
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case "Spanish":
+		prefix = "Hola, "
+
+	case "French":
+		prefix = "Bonjour, "
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
+}
+
+func main() {
+	fmt.Println(Hello("word", "english"))
+}
